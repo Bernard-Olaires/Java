3be108bee559f080bb997cff0aa811d0e9634a05
Bernard Olaires <bolairesjr@gmail.com>
2022-11-03 13:54:09 -0700

    Order&Items

---
+++ java/week_2/Orders&Items/Order.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Order {
+
+    // memeber variables
+    public String name; // default value is null
+    public double total; // default value 0
+    public boolean ready; // default value false 
+
+    public ArrayList<Item> items = new ArrayList<Item>();
+}
