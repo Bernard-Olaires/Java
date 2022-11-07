71f28868c79c06c6f94c576bb33bab2271d699e7
Bernard Olaires <bolairesjr@gmail.com>
2022-11-06 17:56:08 -0800

    CarAndDriver

---
+++ java/week_2/CarAndDriver/Car.java
@@ -0,0 +1,10 @@
+class Car {
+
+    private int gas = 10;
+
+    public int getGas() {
+        return gas;
+    }
+
+    
+}
---
+++ java/week_2/CarAndDriver/Driver.java
@@ -0,0 +1,16 @@
+class Driver extends Car {
+
+    public void drive() {
+        Car.gas -= 1;
+        System.out.println("You are now driving the car.")
+    }
+
+    public void booster() {
+        Car.gas -= 3;
+        System.out.println("You have used a booster!");
+    }
+
+    public void refuel() {
+        Car.gas +=
+    }
+}
