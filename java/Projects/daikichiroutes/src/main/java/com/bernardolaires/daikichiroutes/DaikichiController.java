ca973a3a9bf6736352a689a8c797058f5e331a07
Bernard Olaires <bolairesjr@gmail.com>
2022-11-23 17:22:03 -0800

    Daikichi Path Variables coding dojo assignment. Added two new routes implementing the @PathVariables

--- java/Projects/daikichiroutes/src/main/java/com/bernardolaires/daikichiroutes/DaikichiController.java
+++ java/Projects/daikichiroutes/src/main/java/com/bernardolaires/daikichiroutes/DaikichiController.java
@@ -1,12 +1,14 @@
 package com.bernardolaires.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/daikichi")
 public class DaikichiController {
 	
-	@RequestMapping("/daikichi")
+	@RequestMapping("")
 	public String greeting() {
 		return "Welcome";
 	}
@@ -20,4 +22,23 @@ public class DaikichiController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable String city) {
+		return "<h1 style='color: green'>Congratulations!<h1>, You will soon travel to " + city;
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable int number) {
+		if (number % 2 == 0) {
+			return "<h1 style='color:red'>You will take a grand journey in the near future, but be weary of tempting offers.</h1>";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor, but now is a great time to spent time with framily and friends"; 
+		}
+	}
+
+	
+	
+	
 }
