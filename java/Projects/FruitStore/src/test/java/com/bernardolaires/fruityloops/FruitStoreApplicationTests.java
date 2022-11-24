9ef295148e47cfbc661eab1e708e3d7e935cc8fd
Bernard Olaires <bolairesjr@gmail.com>
2022-11-24 13:39:22 -0800

    pushing FruityLoops coding dojo core assignment

---
+++ java/Projects/FruitStore/src/test/java/com/bernardolaires/fruityloops/FruitStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardolaires.fruityloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
